fca8f6f1f197fdefb50f6cad61c3a2797f824143
asjadishrat17 <asjadishrat17@gmail.com>
2019-11-23 23:59:13 +0500

    dynamic django website work still in process

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
 	path('',views.home, name='home'),
-	path('/layout',views.layout, name='layout'),
+	path('layout',views.layout, name='layout'),
 
 ]
--- travello/models.py
+++ travello/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
+class destination(models.Model):
+    name = models.CharField(max_length=100)
+    img  = models.ImageField(upload_to='pics')
+    desc = models.TextField()
+    price = models.IntegerField()
+    special = models.BooleanField(default=False)
 # Create your models here.
